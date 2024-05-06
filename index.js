bea00146739b948e6228bcd658482fddeeff96fe
Jorge Leiva López <jorgeleival@MacBook-Air-de-Jorge.local>
2024-05-06 05:12:46 -0400

    :tada: Revisión desafío Welcome World

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import router from "./routes/router.js";
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Routes
+app.use("/", router);
+
+// Escuchar el puerto
+app.listen(PORT, () => console.log(`Listening on port http://localhost:${PORT}`));
